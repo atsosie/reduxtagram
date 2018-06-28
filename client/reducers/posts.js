3213931a7e982ac6b2b59865e22840281b66fb7a
Amber Tsosie <atxambra@gmail.com>
2018-06-28 13:23:00 -0700

    (Video 12) Added functionality to 'like' button in photo caption, so reducer runs and state is updated on button click.

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,9 +1,20 @@
 // The first time this function runs, state will be equal to nothing,
 // so we're setting initial state with an empty array.
 function posts(state = [], action) {
-  console.log('The post will change');
-  console.log(state, action);
-  return state;
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      console.log('Incrementing likes');
+
+      const idx = action.index;
+
+      return [
+        ...state.slice(0, idx), // everything up to the one we're updating
+        { ...state[idx], likes: state[idx].likes + 1 },
+        ...state.slice(idx + 1) // everything after the one we're updating
+      ];
+    default:
+      return state;
+  }
 }
 
 export default posts;
