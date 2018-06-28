f88cc8350b7b0c2cf5e99b62f4bca27978816295
Amber Tsosie <atxambra@gmail.com>
2018-06-28 13:47:27 -0700

    (Video 13) Set up view for Single Photo component (comments not working yet).

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
-    return <div className="single">I'm the single div</div>;
+    // give us the index of the post
+    const idx = this.props.posts.findIndex(
+      (post) => post.code === this.props.params.postId
+    );
+
+    // give us the post
+    const post = this.props.posts[idx];
+
+    return (
+      <div className="single-photo">
+        <Photo idx={idx} post={post} {...this.props} />
+        <Comments />
+      </div>
+    );
   }
 });
 
